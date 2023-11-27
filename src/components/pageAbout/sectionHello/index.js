2d79d33e181675e751ba4f25df487e499610abf4
Atom Baytovich <atombaytovich@gmail.com>
2023-11-27 21:57:52 +0300

    dev tablet

--- src/components/pageAbout/sectionHello/index.js
+++ src/components/pageAbout/sectionHello/index.js
@@ -5,7 +5,7 @@ export const SectionHello = () => {
     return (
         <section className={style.sectionHello}>
             <img src="../../assets/main/nemo-tishka.png" className={style.helloImg} alt="Любимые немо и тишка"></img>
-            <div className={style.hello}>
+            {/* <div className={style.hello}> */}
                 <div className={style.helloLetsgo}>
                     <div className={`${style.custom} helloLetsgo__container`}>
                         <h1>Делись своими чувствами и эмоциями здесь ❤️</h1>
@@ -14,7 +14,7 @@ export const SectionHello = () => {
                     <div className={style.custom}>
                         <p>Пиши - Делись - Мечтай</p>
                     </div>
-                </div>
+                {/* </div> */}
                 <div className={style.buttons}>
                     <Button><p className={style.buttonsOne}>Присоединиться</p></Button>
                     <Button backgroundColor="#504F75"><p className={style.buttonsTwo}>О нас</p></Button>
