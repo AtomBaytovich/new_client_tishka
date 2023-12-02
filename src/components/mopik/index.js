c7a39170a36e87da2a492fc4157546cff1a2d2b0
Atom Baytovich <atombaytovich@gmail.com>
2023-12-02 20:43:47 +0300

    styled

--- src/components/mopik/index.js
+++ src/components/mopik/index.js
@@ -5,28 +5,26 @@ import { WriteCommentAndViews } from "./social";
 export const Mopik = ({ id, title, text, countLikes, countComment, countView }) => {
     const [nextView, setNextView] = useState(false)
     const [tapCount, setTapCount] = useState(0);
+    const [idTimeout, setIdTimeout] = useState(undefined)
 
     const handleTap = () => {
         // Увеличиваем счетчик тапов на 1
-        setTapCount((tapCount) => tapCount + 1);
-        let id;
+        setTapCount(tapCount + 1);
         if (tapCount < 1) {
             let id = setTimeout(() => {
                 setTapCount(0);
-            }, 1000);
-            console.log(id)
+            }, 800);
+            setIdTimeout(id)
         }
 
         // Если пользователь нажал дважды, совершаем действие
         if (tapCount >= 1) {
             // Выполняем необходимое действие
-            console.log('Действие выполнено!');
             setNextView(!nextView)
             // Сбрасываем счетчик тапов
-            clearTimeout(id)
-            return setTapCount((tapCount) => tapCount = 0);
+            clearTimeout(idTimeout)
+            return setTapCount(0);
         }
-
     };
 
     const getMi = () => {
@@ -36,36 +34,26 @@ export const Mopik = ({ id, title, text, countLikes, countComment, countView }) 
 
     return (
         <div className={style.mopik} id={id}>
-            <div className={style.data}>
+            <div className={style.data} style={nextView ? { borderRadius: "10px 0px 0px 10px" } : null}>
                 <div className={style.written} onClick={() => handleTap()}>
                     <p className={style.title}>{title}</p>
                     <p className={style.text}>{text}</p>
-
-                    {/* {
-                        nextView
-                            ?
-                            <p className={style.next} onClick={() => setNextView(false)}>Скрыть</p>
-                            :
-                            undefined
-                    } */}
-
-                    {/* <p className={style.next} onClick={() => setNextView(!nextView)}>Смотреть далее</p> */}
                 </div>
-                {nextView ? (<>
+                {nextView ? (
                     <WriteCommentAndViews />
-                </>
                 ) : undefined}
             </div>
-
-            {/* <div className={style.meta}>
-                <div className={style.like}>
-                    <img src="./assets/smiles/mopik/like.png" alt="Сердечко" />
-                </div>
-                <div className={style.comment} onClick={() => setNextView(!nextView)}>
+            {nextView ? (
+                <div className={style.meta}>
+                    <div className={style.like}>
+                        <img src="./assets/smiles/mopik/like.png" alt="Сердечко" />
+                    </div>
+                    {/* <div className={style.comment} onClick={() => setNextView(!nextView)}>
                     <img src="./assets/smiles/mopik/message.png" alt="Конвертик" />
+                    </div> */}
+                    <div className={style.views}>{countView}</div>
                 </div>
-                <div className={style.views}>{countView}</div>
-            </div> */}
+            ) : undefined}
         </div>
     )
 }
